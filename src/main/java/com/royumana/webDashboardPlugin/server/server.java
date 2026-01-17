2463532facc7b48b8c97adc1fedf07cfe6150e88
sharkbot-neko <modoggu12345@outlook.com>
2026-01-17 17:59:01 +0900

    雷をWebから落とせるように

--- src/main/java/com/royumana/webDashboardPlugin/server/server.java
+++ src/main/java/com/royumana/webDashboardPlugin/server/server.java
@@ -7,17 +7,10 @@ import com.royumana.webDashboardPlugin.server.routes.login;
 import com.royumana.webDashboardPlugin.server.routes.players.getPlayers;
 import com.royumana.webDashboardPlugin.server.routes.players.kick;
 import com.royumana.webDashboardPlugin.server.routes.players.players;
+import com.royumana.webDashboardPlugin.server.routes.players.thunder;
 import com.royumana.webDashboardPlugin.server.routes.sidebar;
 import fi.iki.elonen.router.RouterNanoHTTPD;
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class server extends RouterNanoHTTPD {
 
@@ -37,6 +30,7 @@ public class server extends RouterNanoHTTPD {
         addRoute("/players", players.class);
         addRoute("/api/players", getPlayers.class);
         addRoute("/api/kick", kick.class, plugin);
+        addRoute("/api/thunder", thunder.class, plugin);
         addRoute("/sidebar.html", sidebar.class);
 
         setNotFoundHandler(error404.class);
