784e881d59266c80d9eb4194339144b6df04da80
sharkbot-neko <modoggu12345@outlook.com>
2026-01-18 16:37:16 +0900

    Killコマンドを追加

--- src/main/java/com/royumana/webDashboardPlugin/server/server.java
+++ src/main/java/com/royumana/webDashboardPlugin/server/server.java
@@ -7,10 +7,7 @@ import com.royumana.webDashboardPlugin.server.routes.error404;
 import com.royumana.webDashboardPlugin.server.routes.index;
 
 import com.royumana.webDashboardPlugin.server.routes.login;
-import com.royumana.webDashboardPlugin.server.routes.players.getPlayers;
-import com.royumana.webDashboardPlugin.server.routes.players.kick;
-import com.royumana.webDashboardPlugin.server.routes.players.players;
-import com.royumana.webDashboardPlugin.server.routes.players.thunder;
+import com.royumana.webDashboardPlugin.server.routes.players.*;
 import com.royumana.webDashboardPlugin.server.routes.sidebar;
 import fi.iki.elonen.router.RouterNanoHTTPD;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -32,6 +29,7 @@ public class server extends RouterNanoHTTPD {
         addRoute("/login", login.class);
         addRoute("/players", players.class);
         addRoute("/api/players", getPlayers.class);
+        addRoute("/api/player/kill", kill.class, plugin);
         addRoute("/api/kick", kick.class, plugin);
         addRoute("/api/thunder", thunder.class, plugin);
         addRoute("/chat", chat.class);
